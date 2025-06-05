5539bcb6271fb07d4cb90dccc80bf839fec5ed66
Antonio Torres <antoniotorres841@gmail.com>
2025-06-05 10:44:47 -0600

    .

--- services/users/src/config/settings/production.py
+++ services/users/src/config/settings/production.py
@@ -1,39 +1,27 @@
 import os
-
 from config.settings.base import *
-from dotenv import load_dotenv
-
-load_dotenv(Path.joinpath(BASE_DIR, '.env'))
-
 import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['red-social.up.railway.app']
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ['DATABASE_DEFAULT'])
+}
 
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_DEFAULT'))}
-
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://red-social.up.railway.app/', 
-]
-
-
-# Cors DjangoRest
-
+# CORS DjangoRest
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://red-social.up.railway.app", 
-]
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{host}" for host in ALLOWED_HOSTS
+] + ['https://red-social.up.railway.app']
 
-CORS_ORIGIN_WHITELIST = [
-    "https://red-social.up.railway.app",
-]
+CORS_ALLOWED_ORIGINS = [
+    f"https://{host}" for host in ALLOWED_HOSTS
+] + ['https://red-social.up.railway.app']
+
+CORS_ORIGIN_WHITELIST = CORS_ALLOWED_ORIGINS
